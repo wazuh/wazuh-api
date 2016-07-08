7865642aa3192237b8c79fe5f567249ca69ac70f
Jesus Linares <jesuslinares90@gmail.com>
2016-07-08 10:58:51 +0000

    Unit tests: syscheck

--- test/test_syscheck.js
+++ test/test_syscheck.js
@@ -16,13 +16,13 @@ var common = require('./common.js');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-describe('Rootcheck', function() {
+describe('Syscheck', function() {
 
-    describe('GET/rootcheck/:agent_id', function() {
+    describe('GET/syscheck/:agent_id/files', function() {
 
         it('Request', function(done) {
             request(common.url)
-            .get("/rootcheck/000")
+            .get("/syscheck/000/files")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -34,14 +34,14 @@ describe('Rootcheck', function() {
                 res.body.error.should.equal(0);
                 res.body.data.totalItems.should.be.above(0);
                 res.body.data.items.should.be.instanceof(Array)
-                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
                 done();
             });
         });
 
         it('Pagination', function(done) {
             request(common.url)
-            .get("/rootcheck/000?offset=0&limit=1")
+            .get("/syscheck/000/files?offset=0&limit=1")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -52,14 +52,14 @@ describe('Rootcheck', function() {
 
                 res.body.error.should.equal(0);
                 res.body.data.items.should.be.instanceof(Array).and.have.lengthOf(1);
-                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
                 done();
             });
         });
 
         it('Sort', function(done) {
             request(common.url)
-            .get("/rootcheck/000?sort=status")
+            .get("/syscheck/000/files?sort=date&offset=0&limit=10")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -71,15 +71,14 @@ describe('Rootcheck', function() {
                 res.body.error.should.equal(0);
                 res.body.data.totalItems.should.be.above(0);
                 res.body.data.items.should.be.instanceof(Array)
-                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
-                res.body.data.items[0]['status'].should.equal('outstanding');
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
                 done();
             });
         });
 
         it('Search', function(done) {
             request(common.url)
-            .get("/rootcheck/000?search=outstanding")
+            .get("/syscheck/000/files?search=a&offset=0&limit=10")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -91,14 +90,14 @@ describe('Rootcheck', function() {
                 res.body.error.should.equal(0);
                 res.body.data.totalItems.should.be.above(0);
                 res.body.data.items.should.be.instanceof(Array)
-                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
                 done();
             });
         });
 
         it('Params: Bad agent id', function(done) {
             request(common.url)
-            .get("/rootcheck/abc")
+            .get("/syscheck/abc/files")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(400)
@@ -113,7 +112,7 @@ describe('Rootcheck', function() {
 
         it('Errors: No agent', function(done) {
             request(common.url)
-            .get("/rootcheck/9999999")
+            .get("/syscheck/9999999/files")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -125,13 +124,121 @@ describe('Rootcheck', function() {
                 done();
             });
         });
-    });  // GET/rootcheck/:agent_id
 
-    describe('GET/rootcheck/:agent_id/last_scan', function() {
+        it('Filters: Invalid filter', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?random")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+
+                res.body.error.should.equal(604);
+                done();
+            });
+        });
+
+        it('Filters: Invalid filter - Extra field', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?search=added&random")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+
+                res.body.error.should.equal(604);
+                done();
+            });
+        });
+
+        it('Filters: event', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?event=added&offset=0&limit=10")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
+                done();
+            });
+        });
+
+        it('Filters: file', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?file=/var/ossec/etc/ossec.conf&offset=0&limit=10")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.an.integer;
+                res.body.data.items.should.be.instanceof(Array)
+                done();
+            });
+        });
+
+        it('Filters: filetype', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?filetype=file&offset=0&limit=10")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['gid', 'uid', 'file', 'date', 'sha1', 'md5', 'event', 'perm', 'size']);
+                done();
+            });
+        });
+
+        it('Filters: summary', function(done) {
+            request(common.url)
+            .get("/syscheck/000/files?summary=yes&offset=0&limit=10")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['file', 'date', 'event']);
+                done();
+            });
+        });
+
+    });  // GET/syscheck/:agent_id
+
+    describe('GET/syscheck/:agent_id/last_scan', function() {
 
         it('Request', function(done) {
             request(common.url)
-            .get("/rootcheck/000/last_scan")
+            .get("/syscheck/000/last_scan")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -142,14 +249,14 @@ describe('Rootcheck', function() {
 
                 res.body.error.should.equal(0);
                 res.body.data.should.be.an.Object;
-                res.body.data.should.have.properties(['rootcheckEndTime', 'rootcheckTime']);
+                res.body.data.should.have.properties(['syscheckEndTime', 'syscheckTime']);
                 done();
             });
         });
 
         it('Params: Bad agent id', function(done) {
             request(common.url)
-            .get("/rootcheck/abc/last_scan")
+            .get("/syscheck/abc/last_scan")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(400)
@@ -164,7 +271,7 @@ describe('Rootcheck', function() {
 
         it('Errors: No agent', function(done) {
             request(common.url)
-            .get("/rootcheck/9999999/last_scan")
+            .get("/syscheck/9999999/last_scan")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -177,13 +284,14 @@ describe('Rootcheck', function() {
             });
         });
 
-    });  // GET/rootcheck/:agent_id/last_scan
+    });  // GET/syscheck/:agent_id/last_scan
 
-    describe('DELETE/rootcheck/:agent_id', function() {
+    describe('DELETE/syscheck/:agent_id', function() {
 
         it('Request', function(done) {
+            this.timeout(20000);
             request(common.url)
-            .delete("/rootcheck/000")
+            .delete("/syscheck/000")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -200,7 +308,7 @@ describe('Rootcheck', function() {
 
         it('Params: Bad agent id', function(done) {
             request(common.url)
-            .delete("/rootcheck/abc")
+            .delete("/syscheck/abc")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(400)
@@ -215,7 +323,7 @@ describe('Rootcheck', function() {
 
         it('Errors: No agent', function(done) {
             request(common.url)
-            .delete("/rootcheck/9999999")
+            .delete("/syscheck/9999999")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -228,13 +336,14 @@ describe('Rootcheck', function() {
             });
         });
 
-    });  // DELETE/rootcheck/:agent_id
+    });  // DELETE/syscheck/:agent_id
 
-    describe('DELETE/rootcheck', function() {
+    describe('DELETE/syscheck', function() {
 
         it('Request', function(done) {
+            this.timeout(20000);
             request(common.url)
-            .delete("/rootcheck")
+            .delete("/syscheck")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -249,13 +358,13 @@ describe('Rootcheck', function() {
             });
         });
 
-    });  // DELETE/rootcheck
+    });  // DELETE/syscheck
 
-    describe('PUT/rootcheck/:agent_id', function() {
+    describe('PUT/syscheck/:agent_id', function() {
 
         it('Request', function(done) {
             request(common.url)
-            .put("/rootcheck/000")
+            .put("/syscheck/000")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -272,7 +381,7 @@ describe('Rootcheck', function() {
 
         it('Params: Bad agent id', function(done) {
             request(common.url)
-            .put("/rootcheck/abc")
+            .put("/syscheck/abc")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(400)
@@ -287,7 +396,7 @@ describe('Rootcheck', function() {
 
         it('Errors: No agent', function(done) {
             request(common.url)
-            .put("/rootcheck/9999999")
+            .put("/syscheck/9999999")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -299,13 +408,13 @@ describe('Rootcheck', function() {
                 done();
             });
         });
-    });  // PUT/rootcheck/:agent_id
+    });  // PUT/syscheck/:agent_id
 
-    describe('PUT/rootcheck', function() {
+    describe('PUT/syscheck', function() {
 
         it('Request', function(done) {
             request(common.url)
-            .put("/rootcheck")
+            .put("/syscheck")
             .auth(common.credentials.user, common.credentials.password)
             .expect("Content-type",/json/)
             .expect(200)
@@ -320,6 +429,6 @@ describe('Rootcheck', function() {
             });
         });
 
-    });  // PUT/rootcheck
+    });  // PUT/syscheck
 
 });  // Decoders
