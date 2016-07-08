c2a61ac3e22a748026f4600099460202a354cd9f
Jesus Linares <jesuslinares90@gmail.com>
2016-07-08 09:59:41 +0000

    Unit tests: Rootcheck

---
+++ test/test_rootcheck.js
@@ -0,0 +1,325 @@
+/**
+ * API RESTful for OSSEC
+ * Copyright (C) 2015-2016 Wazuh, Inc.All rights reserved.
+ * Wazuh.com
+ *
+ * This program is a free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var common = require('./common.js');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe('Rootcheck', function() {
+
+    describe('GET/rootcheck/:agent_id', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .get("/rootcheck/000")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                done();
+            });
+        });
+
+        it('Pagination', function(done) {
+            request(common.url)
+            .get("/rootcheck/000?offset=0&limit=1")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.items.should.be.instanceof(Array).and.have.lengthOf(1);
+                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                done();
+            });
+        });
+
+        it('Sort', function(done) {
+            request(common.url)
+            .get("/rootcheck/000?sort=status")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                res.body.data.items[0]['status'].should.equal('outstanding');
+                done();
+            });
+        });
+
+        it('Search', function(done) {
+            request(common.url)
+            .get("/rootcheck/000?search=outstanding")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.totalItems.should.be.above(0);
+                res.body.data.items.should.be.instanceof(Array)
+                res.body.data.items[0].should.have.properties(['status', 'oldDay', 'readDay', 'event']);
+                done();
+            });
+        });
+
+        it('Params: Bad agent id', function(done) {
+            request(common.url)
+            .get("/rootcheck/abc")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(600);
+                done();
+            });
+        });
+
+        it('Errors: No agent', function(done) {
+            request(common.url)
+            .get("/rootcheck/9999999")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(1600);
+                done();
+            });
+        });
+    });  // GET/rootcheck/:agent_id
+
+    describe('GET/rootcheck/:agent_id/last_scan', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .get("/rootcheck/000/last_scan")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.should.be.an.Object;
+                res.body.data.should.have.properties(['rootcheckEndTime', 'rootcheckTime']);
+                done();
+            });
+        });
+
+        it('Params: Bad agent id', function(done) {
+            request(common.url)
+            .get("/rootcheck/abc/last_scan")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(600);
+                done();
+            });
+        });
+
+        it('Errors: No agent', function(done) {
+            request(common.url)
+            .get("/rootcheck/9999999/last_scan")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(40);
+                done();
+            });
+        });
+
+    });  // GET/rootcheck/:agent_id/last_scan
+
+    describe('PUT/rootcheck', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/rootcheck")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.should.be.an.string;
+                done();
+            });
+        });
+
+    });  // PUT/rootcheck
+
+    describe('PUT/rootcheck/:agent_id', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/rootcheck/000")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.should.be.an.string;
+                done();
+            });
+        });
+
+        it('Params: Bad agent id', function(done) {
+            request(common.url)
+            .put("/rootcheck/abc")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(600);
+                done();
+            });
+        });
+
+        it('Errors: No agent', function(done) {
+            request(common.url)
+            .put("/rootcheck/9999999")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(40);
+                done();
+            });
+        });
+    });  // PUT/rootcheck/:agent_id
+
+    describe('DELETE/rootcheck', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .delete("/rootcheck")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.should.be.an.string;
+                done();
+            });
+        });
+
+    });  // DELETE/rootcheck
+
+    describe('DELETE/rootcheck/:agent_id', function() {
+
+        it('Request', function(done) {
+            request(common.url)
+            .delete("/rootcheck/000")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.should.be.an.string;
+                done();
+            });
+        });
+
+        it('Params: Bad agent id', function(done) {
+            request(common.url)
+            .delete("/rootcheck/abc")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(400)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(600);
+                done();
+            });
+        });
+
+        it('Errors: No agent', function(done) {
+            request(common.url)
+            .delete("/rootcheck/9999999")
+            .auth(common.credentials.user, common.credentials.password)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'message']);
+                res.body.error.should.equal(1600);
+                done();
+            });
+        });
+
+    });  // DELETE/rootcheck/:agent_id
+
+});  // Decoders
