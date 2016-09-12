23c53b9008bb520bf2afe557f98d3b7763542146
Jesus Linares <jesuslinares90@gmail.com>
2016-09-12 08:25:10 +0000

    Sort agents by status using SQL

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -221,7 +221,6 @@ class Agent:
         # Query
         query = "SELECT {0} FROM agent"
         fields = {'id': 'id', 'name': 'name', 'ip': 'ip', 'status': 'last_keepalive'}
-        sort_by_status_manually = False
         select = ["id", "name", "ip", "last_keepalive"]
         request = {}
 
@@ -257,12 +256,20 @@ class Agent:
                 if sf not in allowed_sort_fields:
                     raise WazuhException(1403, 'Allowed sort fields: {0}. Field: {1}'.format(allowed_sort_fields, sf))
 
-            if 'status' in sort['fields']:
-                sort_by_status_manually = True
-                sort['fields'].remove('status')
+            order_str_fields = []
+            for i in sort['fields']:
+                # Order by status ASC is the same that order by last_keepalive DESC.
+                if i == 'status':
+                    if sort['order'] == 'asc':
+                        str_order = "desc"
+                    else:
+                        str_order = "asc"
+                else:
+                    str_order = sort['order']
+                order_str_fields.append('{0} {1}'.format(fields[i], str_order))
 
             if sort['fields']:
-                query += ' ORDER BY ' + ','.join(['{0} {1}'.format(fields[i], sort['order']) for i in sort['fields']])
+                query += ' ORDER BY ' + ','.join(order_str_fields)
         else:
             query += ' ORDER BY id ASC'
 
@@ -295,9 +302,6 @@ class Agent:
                 data_tuple['status'] = Agent.calculate_status(lastKeepAlive)
 
             data['items'].append(data_tuple)
-
-        if sort_by_status_manually:
-            data['items'] = sort_array(data['items'], ['status'], sort['order'])
 
         return data
 
