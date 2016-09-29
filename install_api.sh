4fd2e13ae41db280705f8b5988ded776b2c2c447
Jesus Linares <jesuslinares90@gmail.com>
2016-09-29 15:14:33 +0000

    typo

--- install_api.sh
+++ install_api.sh
@@ -122,6 +122,7 @@ required_packages() {
     print "\t\tsudo apt-get install -y nodejs"
 
     print "\nRed Hat, CentOS and Fedora:"
+    print "\tsudo yum install epel-release"
     print "\tsudo yum install -y python-pip"
     print "\tNodeJS 4.x or newer:"
     print "\t\tcurl --silent --location https://rpm.nodesource.com/setup_4.x | bash -"
