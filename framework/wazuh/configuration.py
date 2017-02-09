5e8eaa6e856927d535475e3642816e9384b72d51
Jesus Linares <jesuslinares90@gmail.com>
2017-02-09 00:49:02 +0000

    Update get_ossec_conf without xmljson

--- framework/wazuh/configuration.py
+++ framework/wazuh/configuration.py
@@ -3,67 +3,158 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-
-import subprocess
-import os
 from xml.etree.ElementTree import fromstring
 from wazuh.exception import WazuhException
 from wazuh import common
 
-import_problem = None
+conf_sections = {
+    'active-response': { 'type': 'duplicate', 'list_options': [] },
+    'command': { 'type': 'duplicate', 'list_options': [] },
+    'agentless': { 'type': 'duplicate', 'list_options': [] },
+    'localfile': { 'type': 'duplicate', 'list_options': [] },
+    'remote': { 'type': 'duplicate', 'list_options': [] },
+    'syslog_output': { 'type': 'duplicate', 'list_options': [] },
 
-try:
-    from xmljson import gdata as xml_json
-except Exception as e:
-    import_problem = e
+    'alerts': { 'type': 'simple', 'list_options': [] },
+    'client': { 'type': 'simple', 'list_options': [] },
+    'database_output': { 'type': 'simple', 'list_options': [] },
+    'email_alerts': { 'type': 'simple', 'list_options': [] },
+    'reports': { 'type': 'simple', 'list_options': [] },
+    'global': {
+        'type': 'simple',
+        'list_options': ['white_list']
+    },
+
+    'open-scap': {
+        'type': 'mix',
+        'list_options': ['content']
+    },
+    'rootcheck': {
+        'type': 'mix',
+        'list_options': ['rootkit_files', 'rootkit_trojans', 'windows_audit', 'system_audit', 'windows_apps', 'windows_malware']
+    },
+    'ruleset': {
+        'type': 'mix',
+        'list_options':  ['include', 'rule', 'rule_dir', 'decoder', 'decoder_dir', 'list', 'rule_exclude', 'decoder_exclude']
+    },
+    'syscheck': {
+        'type': 'mix',
+        'list_options': ['directories', 'ignore', 'nodiff']
+    }
+}
 
 
-def __merge_json_array(json_array):
-    if isinstance(json_array, list):
-        new_json = {}
-        for element in json_array:
-            new_json.update(element)
-        return new_json
-    return json_array
+def _insert(json, section_name, key, value):
+    if not value:
+        return
 
-
-def __process_ossecconf(json_conf):
-
-    for element in json_conf:
-        if not isinstance(json_conf[element], list):
-            for item in json_conf[element]:
-                if not isinstance(json_conf[element][item], list):
-                    if '$t' in json_conf[element][item]:
-                        json_conf[element][item] = json_conf[element][item]['$t']
-                else:
-                    clean_list = []
-                    for i in range(len(json_conf[element][item])):
-                        if not isinstance(json_conf[element][item][i], list):
-                            if '$t' in json_conf[element][item][i]:
-                                clean_list.insert(len(clean_list), json_conf[element][item][i]['$t'])
-                    json_conf[element][item] = clean_list
+    if key in json:
+        old_value = json[key]
+        # The old value is a list -> append new value
+        if type(old_value) is list:
+            json[key].append(value)
+        # The old value is simple -> update
         else:
-            for i in range(len(json_conf[element])):
-                if not isinstance(json_conf[element][i], list):
-                    for item in json_conf[element][i]:
-                        if '$t' in json_conf[element][i][item]:
-                            json_conf[element][i][item] = json_conf[element][i][item]['$t']
-    return json_conf
+            json[key] = value
+    else:
+        # if key in list_options -> store as list
+        if section_name in conf_sections and key in conf_sections[section_name]['list_options']:
+            json[key] = [value]
+        else:
+            json[key] = value
 
 
-def __prepare_ossecconf(json_conf):
-    json_conf = "<root>" + json_conf + "</root>"
-    return json_conf
+def _insert_section(json, section_name, section_json):
+    # Duplicated sections -> always list
+    if section_name in conf_sections and conf_sections[section_name]['type'] == 'duplicate':
+        if section_name in json:
+            json[section_name].append(section_json)
+        else:
+            json[section_name] = [section_json]
+    # Mix sections:
+    #   - If value in list_options -> append
+    #   - else update value (final result: last value)
+    elif section_name in conf_sections and conf_sections[section_name]['type'] == 'mix':
+        if section_name in json:
+            old_json = json[section_name]
+            for value in section_json:
+                if value in conf_sections[section_name]['list_options']:
+                    old_json[value].extend(section_json[value])
+                else:
+                    old_json[value] = section_json[value]
+            json[section_name] = old_json
+        else:
+            json[section_name] = section_json
+    # Simple section: Update values and copy new ones
+    else:
+        if section_name in json:
+            old_json = json[section_name]
+            for value in section_json:
+                old_json[value] = section_json[value]
+            json[section_name] = old_json
+        else:
+            json[section_name] = section_json
 
+def _read_option(section, opt):
+    opt_name = opt.tag.lower()
 
-def __unify_ossecconf(json_conf):
-    json_conf["root"]["ossec_config"] = __merge_json_array(json_conf["root"]['ossec_config'])
-    json_conf["root"]["ossec_config"]["global"] = __merge_json_array(json_conf["root"]['ossec_config']["global"])
-    json_conf["root"]["ossec_config"]["syscheck"] = __merge_json_array(json_conf["root"]['ossec_config']["syscheck"])
-    json_conf["root"]["ossec_config"]["rootcheck"] = __merge_json_array(json_conf["root"]['ossec_config']["rootcheck"])
-    json_conf = json_conf["root"]
-    json_conf = __process_ossecconf(json_conf['ossec_config'])
-    return json_conf
+    if section == 'open-scap':
+        if opt.attrib:
+            opt_value = {}
+            for a in opt.attrib:
+                opt_value[a] = opt.attrib[a]
+            # profiles
+            profiles_list = []
+            for profiles in opt.getchildren():
+                profiles_list.append(profiles.text)
+
+            if profiles_list:
+                opt_value['profiles'] = profiles_list
+        else:
+            opt_value = opt.text
+    elif section == 'syscheck' and opt_name == 'directories':
+        opt_value = []
+
+        json_attribs = {}
+        for a in opt.attrib:
+            json_attribs[a] = opt.attrib[a]
+
+        for path in opt.text.split(','):
+            json_path = {}
+            json_path = json_attribs.copy()
+            json_path['path'] = path.strip()
+            opt_value.append(json_path)
+    else:
+        if opt.attrib:
+            opt_value = {}
+            opt_value['item'] = opt.text
+            for a in opt.attrib:
+                opt_value[a] = opt.attrib[a]
+        else:
+            opt_value = opt.text
+
+    return opt_name, opt_value
+
+def _conf2json(xml_conf):
+    root_json = {}
+
+    for root in xml_conf.getchildren():
+        if root.tag.lower() == "ossec_config":
+            for section in root.getchildren():
+                section_name = 'open-scap' if section.tag.lower() == 'wodle' else section.tag.lower()
+                section_json = {}
+
+                for option in section.getchildren():
+                    option_name, option_value = _read_option(section_name, option)
+                    if type(option_value) is list:
+                        for ov in option_value:
+                            _insert(section_json, section_name, option_name, ov)
+                    else:
+                        _insert(section_json, section_name, option_name, option_value)
+
+                _insert_section(root_json, section_name, section_json)
+
+    return root_json
 
 
 def get_ossec_conf(section=None, field=None):
@@ -75,18 +166,21 @@ def get_ossec_conf(section=None, field=None):
     :return: ossec.conf as dictionary.
     """
 
-    if import_problem is not None:
-        raise WazuhException(1001, import_problem)
-    else:
-        try:
-            with open(common.ossec_conf, 'r') as f_ossec:
-                read_conf = f_ossec.read()
-                read_conf = read_conf.replace(" -- ", " -INVALID_CHAR ")  # Remove invalid characters for XML Parser
-                read_conf = __prepare_ossecconf(read_conf)
-                json_conf = xml_json.data(fromstring(read_conf))
-                data = __unify_ossecconf(json_conf)
-        except:
-            raise WazuhException(1101)
+    try:
+        # wrap the data
+        f = open(common.ossec_conf)
+        txt_data = f.read()
+        txt_data = txt_data.replace(" -- ", " -INVALID_CHAR ")
+        f.close()
+        txt_data = '<root_tag>' + txt_data + '</root_tag>'
+
+        # Read XML
+        xml_data = fromstring(txt_data)
+
+        # Parse XML to JSON
+        data = _conf2json(xml_data)
+    except:
+        raise WazuhException(1101)
 
     if section:
         try:
@@ -101,32 +195,3 @@ def get_ossec_conf(section=None, field=None):
             raise WazuhException(1103)
 
     return data
-
-
-#def check():
-#    """
-#    Checks OSSEC configuration (ossec-logtest).
-#
-#    :return: "OK" if there is no problem, error logs otherwise.
-#    """
-#
-#    p = subprocess.Popen([common.ossec_logtest, "-t"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#    (output, err) = p.communicate()
-#
-#    lines = err.split(os.linesep)
-#    error_line = 0
-#    for l in lines:
-#        if "error" in l.lower():
-#            break
-#        else:
-#            error_line += 1
-#
-#    if err:
-#        if "Error" in err:
-#            data = "{0}".format(lines[error_line:-1])
-#        else:
-#            data = "OK"
-#    else:
-#        raise WazuhException(1100)
-#
-#    return data
