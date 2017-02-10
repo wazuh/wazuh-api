0cd81257175cabe3771c740378b139208dc87c44
Jesus Linares <jesuslinares90@gmail.com>
2017-02-10 17:53:56 +0000

    Update python examples

--- framework/examples/rules2csv.py
+++ framework/examples/rules2csv.py
@@ -15,7 +15,7 @@
 #  Optional:
 #  - Configure the python path. Example for python27 package in Centos6
 #    - export PATH=$PATH:/opt/rh/python27/root/usr/bin
-#    - export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/python27/root/usr/bin/python
+#    - export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/python27/root/usr/lib64
 #  - Use the framework sqlite lib
 #    - export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/ossec/api/framework/lib
 
