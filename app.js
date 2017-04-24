962bd82050827d20966afb4ce2fa9bf6d46f4fb1
Jesus Linares <jesuslinares90@gmail.com>
2017-04-24 15:03:55 +0000

    Increase version

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ python_bin = '';
 /********************************************/
 /* Config APP
 /********************************************/
-current_version = "v2.0.0";
+current_version = "v2.0.1";
 
 if (process.argv.length == 3 && process.argv[2] == "-f")
     logger.set_foreground();
