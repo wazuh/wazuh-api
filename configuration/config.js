751a75e80744210c24e6f1e1479a82143f7145ed
Marta Gómez Macías <marta@wazuh.com>
2017-12-07 01:31:51 +0100

    Add option in configuration to force the use of authd

--- configuration/config.js
+++ configuration/config.js
@@ -53,7 +53,10 @@ config.python = [
     }
 ];
 // Shared library path
-config.ld_library_path = config.ossec_path + "/api/framework/lib"
+config.ld_library_path = config.ossec_path + "/api/framework/lib";
+
+// Option to force the use of authd to remove and add agents
+config.use_only_authd = false;
 
 /************************* SSL OPTIONS ****************************************/
 // SSL protocol
