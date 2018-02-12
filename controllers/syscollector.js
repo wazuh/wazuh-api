0939adfd70fad0f8657cf1fbaa77f45561f63983
Pedro Sanchez <pedro@wazuh.com>
2018-02-12 06:06:22 +0100

    Removing unneeded filters for hardware endpoint

--- controllers/syscollector.js
+++ controllers/syscollector.js
@@ -52,10 +52,6 @@ router.get('/:agent_id/os', function(req, res) {
  * @apiGroup Syscollector
  *
  * @apiParam {Number} agent_id Agent ID.
- * @apiParam {Number} [offset] First element to return in the collection.
- * @apiParam {Number} [limit=500] Maximum number of elements to return.
- * @apiParam {String} [sort] Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to ascending or descending order.
- * @apiParam {String} [search] Looks for elements with the specified string.
  *
  * @apiDescription Returns the agent's hardware info
  *
@@ -68,10 +64,7 @@ router.get('/:agent_id/hardware', function(req, res) {
 
     var data_request = {'function': '/syscollector/:agent_id/hardware', 'arguments': {}};
     
-    var filters = {'offset': 'numbers', 'limit': 'numbers', 'sort':'sort_param',
-                   'search':'search_param', 'select':'select_param',
-                    'ram_free': 'numbers', 'ram_total': 'numbers', 'cpu_cores': 'numbers', 'cpu_mhz': 'alphanumeric_param', 
-                    'cpu_name': 'alphanumeric_param', 'board_serial': 'alphanumeric_param','format': 'alphanumeric_param'};
+    var filters = {'select':'select_param'};
 
 
     if (!filter.check(req.params, {'agent_id':'numbers'}, req, res))  // Filter with error
@@ -84,27 +77,7 @@ router.get('/:agent_id/hardware', function(req, res) {
     data_request['arguments']['filters']  = {};
 
     if ('select' in req.query)
-        data_request['arguments']['select'] = filter.select_param_to_json(req.query.select)
-    if ('offset' in req.query)
-        data_request['arguments']['offset'] = req.query.offset;
-    if ('limit' in req.query)
-        data_request['arguments']['limit'] = req.query.limit;
-    if ('sort' in req.query)
-        data_request['arguments']['sort'] = filter.sort_param_to_json(req.query.sort);
-    if ('search' in req.query)
-        data_request['arguments']['search'] = filter.search_param_to_json(req.query.search);
-    if ('ram_free' in req.query)
-        data_request['arguments']['filters']['ram_free'] = req.query.ram_free
-    if ('ram_total' in req.query)
-        data_request['arguments']['filters']['ram_total'] = req.query.ram_total
-    if ('cpu_cores' in req.query)
-        data_request['arguments']['filters']['cpu_cores'] = req.query.cpu_cores
-    if ('cpu_mhz' in req.query)
-        data_request['arguments']['filters']['cpu_mhz'] = req.query.cpu_mhz
-    if ('cpu_name' in req.query)
-        data_request['arguments']['filters']['cpu_name'] = req.query.cpu_name
-    if ('board_serial' in req.query)
-        data_request['arguments']['filters']['board_serial'] = req.query.board_serial        
+        data_request['arguments']['select'] = filter.select_param_to_json(req.query.select)     
         
         
     execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
@@ -170,7 +143,7 @@ router.get('/:agent_id/packages', function(req, res) {
 
 /**
  * @api {get} /syscollector/os Get OS info of all agents
- * @apiName GetOS
+ * @apiName GetPackages
  * @apiGroup Syscollector
  *
  * @apiParam {Number} agent_id Agent ID.
@@ -228,7 +201,7 @@ router.get('/packages', function(req, res) {
 
 /**
  * @api {get} /syscollector/packages Get os info of all agents
- * @apiName GetPackages
+ * @apiName GetOS
  * @apiGroup Syscollector
  *
  * @apiParam {Number} agent_id Agent ID.
@@ -247,7 +220,11 @@ router.get('/os', function(req, res) {
     logger.debug(req.connection.remoteAddress + " GET /syscollector/os");
 
     var data_request = {'function': '/syscollector/os', 'arguments': {}};
-    var filters = {'os_name': 'alphanumeric_param', 'architecture': 'alphanumeric_param', 'limit': 'numbers', 'offset': 'numbers'};
+    
+    var filters = {'offset': 'numbers', 'limit': 'numbers', 'sort':'sort_param',
+                   'search':'search_param', 'select':'select_param',
+                   'os_name': 'alphanumeric_param', 'architecture': 'alphanumeric_param',
+                    'os_version': 'alphanumeric_param','version': 'alphanumeric_param','release': 'alphanumeric_param' };
 
 
     if (!filter.check(req.params, {'agent_id':'numbers'}, req, res))  // Filter with error
@@ -259,6 +236,10 @@ router.get('/os', function(req, res) {
     data_request['arguments']['agent_id'] = req.params.agent_id;
     data_request['arguments']['filters']  = {};
 
+    if ('select' in req.query)
+        data_request['arguments']['select'] = filter.select_param_to_json(req.query.select)
+    if ('search' in req.query)
+        data_request['arguments']['search'] = filter.search_param_to_json(req.query.search);        
     if ('offset' in req.query)
         data_request['arguments']['offset'] = offset;
     if ('limit' in req.query)
@@ -267,7 +248,14 @@ router.get('/os', function(req, res) {
         data_request['arguments']['filters']['architecture'] = req.query.architecture
     if ('os_name' in req.query)
         data_request['arguments']['filters']['os_name'] = req.query.os_name
-
+    if ('os_version' in req.query)
+        data_request['arguments']['filters']['os_version'] = req.query.os_version
+    if ('version' in req.query)
+        data_request['arguments']['filters']['version'] = req.query.version
+    if ('release' in req.query)
+        data_request['arguments']['filters']['release'] = req.query.release         
+        
+             
     execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
 })
 
@@ -295,7 +283,7 @@ router.get('/hardware', function(req, res) {
     var filters = {'offset': 'numbers', 'limit': 'numbers', 'sort':'sort_param',
                    'search':'search_param', 'select':'select_param',
                     'ram_free': 'numbers', 'ram_total': 'numbers', 'cpu_cores': 'numbers', 'cpu_mhz': 'alphanumeric_param', 
-                    'cpu_name': 'alphanumeric_param', 'board_serial': 'alphanumeric_param','format': 'alphanumeric_param'};
+                    'cpu_name': 'alphanumeric_param', 'board_serial': 'alphanumeric_param'};
 
 
     if (!filter.check(req.params, {'agent_id':'numbers'}, req, res))  // Filter with error
