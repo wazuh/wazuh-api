e33662f7b1ee6741671121628a294aea3027b6e6
Lifka <javierizquierdovera@gmail.com>
2018-06-22 12:35:41 +0200

    Add API logs rotation

--- helpers/logger.js
+++ helpers/logger.js
@@ -11,10 +11,15 @@
 
 var moment = require('moment');
 var fs = require('fs');
+var path = require('path');
+var rfs = require('rotating-file-stream');
 
 var tag = "WazuhAPI";
-var f_log = config.log_path;
+var absolute_path_log = config.log_path;
+var file_log = path.basename(config.log_path);
+var path_log = path.dirname(config.log_path);
 var foreground = false;
+
 var LEVEL_DISABLED = 0;
 var LEVEL_INFO = 1;
 var LEVEL_WARNING = 2;
@@ -22,7 +27,7 @@ var LEVEL_ERROR = 3;
 var LEVEL_DEBUG = 4;
 
 var logger_level = LEVEL_INFO;
-switch(config.logs) {
+switch (config.logs){
     case "INFO", "info":
         logger_level = LEVEL_INFO;
         break;
@@ -42,7 +47,7 @@ switch(config.logs) {
         logger_level = LEVEL_INFO;
 }
 
-exports.set_foreground = function() {
+exports.set_foreground = function (){
     foreground = true;
 }
 
@@ -51,31 +56,50 @@ function header(){
 }
 
 function write_log(msg){
-    if(foreground)
+    if (foreground)
         console.log(msg);
-    fs.appendFile(f_log, msg + "\n", {'mode': 0o640}, function(err) {
-        if(err) {
+    fs.appendFile(absolute_path_log, msg + "\n", { 'mode': 0o640 }, function (err) {
+        if (err) {
             return console.error(err);
         }
     });
 }
 
-exports.log = function(message) {
-    if(logger_level >= LEVEL_INFO)
+exports.log = function(message){
+    if (logger_level >= LEVEL_INFO)
         write_log(header() + message);
 }
 
-exports.warning = function(message) {
-    if(logger_level >= LEVEL_WARNING)
+exports.warning = function(message){
+    if (logger_level >= LEVEL_WARNING)
         write_log(header() + message);
 }
 
-exports.error = function(message) {
-    if(logger_level >= LEVEL_ERROR)
+exports.error = function(message){
+    if (logger_level >= LEVEL_ERROR)
         write_log(header() + message);
 }
 
-exports.debug = function(message) {
-    if(logger_level >= LEVEL_DEBUG)
+exports.debug = function(message){
+    if (logger_level >= LEVEL_DEBUG)
         write_log(header() + message);
 }
+
+function pad(num) {
+    return (num > 9 ? "" : "0") + num;
+}
+
+function generator(time, index){
+    if (!time)
+        return path_log + "/" + file_log;
+
+    var month = moment.monthsShort(time.getMonth());
+    var day = pad(time.getDate());
+    var year = pad(time.getFullYear());
+
+    return path_log + "/archives/" + year + "/" + month + "/api-archive-" + day + ".log-" + index;
+}
+
+var stream = rfs(generator, {
+    interval: '1d'
+});
