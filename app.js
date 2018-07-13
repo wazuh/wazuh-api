e9e4dccf40843a9c2b782292686d6b1c202cb3b8
Marta Gómez Macías <marta@wazuh.com>
2018-07-12 18:53:58 -0700

    Fix show last correct user when there is an auth error

--- app.js
+++ app.js
@@ -143,11 +143,13 @@ if (config.basic_auth.toLowerCase() == "yes"){
     app.use(auth.connect(auth_secure));
 
     auth_secure.on('fail', (result, req) => {
+        logger.set_user(result.user);
         var log_msg = "[" + req.connection.remoteAddress + "] " + "User: \"" + result.user + "\" - Authentication failed.";
         logger.log(log_msg);
     });
 
     auth_secure.on('error', (error, req) => {
+        logger.set_user("");
         var log_msg = "[" + req.connection.remoteAddress + "] Authentication error: " + error.code + " - " + error.message;
         logger.log(log_msg);
     });
