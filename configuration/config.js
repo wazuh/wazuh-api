ac6c664113f2c1db47e06d0f011b2dcd083e5436
Lifka <javierizquierdovera@gmail.com>
2018-07-31 09:41:20 +0200

    Set default log level to info

--- configuration/config.js
+++ configuration/config.js
@@ -27,7 +27,7 @@ config.BehindProxyServer = "no";
 // Advanced configuration
 
 // Values for API log: disabled, info, warning, error, debug (each level includes the previous level).
-config.logs = "debug";
+config.logs = "info";
 // Cross-origin resource sharing. Values: yes, no.
 config.cors = "yes";
 // Cache (time in milliseconds)
