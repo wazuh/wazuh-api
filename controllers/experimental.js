ca5e85c7cd8c48fe53361a82d3c46db4620db81d
Marta Gómez Macías <marta@wazuh.com>
2018-09-03 20:23:03 +0200

    Revert "Move /active-response/:agent_id to experimental"
    This reverts commit e9a245664e90109f76e315bad5f17828ba6ac343.

--- controllers/experimental.js
+++ controllers/experimental.js
@@ -679,36 +679,4 @@ router.get('/ciscat/results', function (req, res) {
     execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
 })
 
-/**
- * @api {put} /experimental/active-response/:agent_id Run an AR command in the agent
- * @apiName PutARAgentIdCommand
- * @apiGroup Command
- *
- * @apiParam {Number} agent_id Agent ID.
- * @apiParam {String} command Command.
- * @apiParam {Boolean} Custom Custom.
- * @apiParam {Arguments} Arguments Command arguments.
- *
- * @apiDescription Runs an Active Response command on a specified agent
- *
- * @apiExample {curl} Example usage:
- *     curl -u foo:bar -k -X PUT -d '{"command":"restart-ossec0", "arguments": ["-", "null", "(from_the_server)", "(no_rule_id)"]}' -H 'Content-Type:application/json' "https://127.0.0.1:55000/experimental/active-response/001?pretty"
- */
-router.put('/active-response/:agent_id', function(req, res) {
-    logger.debug(req.connection.remoteAddress + " PUT /experimental/active-response/:agent_id");
-
-    var data_request = {'function': '/PUT/experimental/active-response/:agent_id', 'arguments': {}};
-
-    // ToDo: Add argments
-    if (!filter.check(req.params, {'agent_id':'numbers'}, req, res))  // Filter with error
-        return;
-
-    data_request['arguments']['agent_id'] = req.params.agent_id;
-    data_request['arguments']['command'] = req.body.command;
-    data_request['arguments']['custom'] = req.body.custom;
-    data_request['arguments']['arguments'] = req.body.arguments;
-
-    execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
-})
-
 module.exports = router;
