c325ae73de0cc02df2da9efa215c899dab991028
Marta Gómez Macías <marta@wazuh.com>
2019-01-21 13:27:51 +0100

    Update headers in javascript files

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -1,6 +1,6 @@
 /**
- * API RESTful for OSSEC
- * Copyright (C) 2015-2016 Wazuh, Inc.All rights reserved.
+ * API RESTful for Wazuh
+ * Copyright (C) 2015-2019 Wazuh, Inc. All rights reserved.
  * Wazuh.com
  *
  * This program is a free software; you can redistribute it
