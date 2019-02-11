d10375e24ba31f3af9b97e31a3eec94c472441e7
Daniel Ruiz Capilla <crd1985@gmail.com>
2019-02-11 17:40:10 +0100

    Change endpoint to use policy id

--- controllers/configuration_assessment.js
+++ controllers/configuration_assessment.js
@@ -13,7 +13,7 @@
 var router = require('express').Router();
 
 /**
- * @api {get} /configuration_assessment/:agent_id Get configuration_assessment database
+ * @api {get} /configuration-assessment/:agent_id Get configuration assessment database
  * @apiName GetPMAgent
  * @apiGroup Info
  *
@@ -28,25 +28,25 @@ var router = require('express').Router();
  * @apiParam {String} [search] Looks for elements with the specified string.
  * @apiParam {String} [q] Query to filter results by. This is specially useful to filter by total checks passed, failed or total score (fields pass, fail, score).
  *
- * @apiDescription Returns the configuration_assessment database of an agent.
+ * @apiDescription Returns the configuration assessment database of an agent.
  *
  * @apiExample {curl} Example usage:
- *     curl -u foo:bar -k -X GET "https://127.0.0.1:55000/configuration_assessment/000?offset=0&limit=2&q=pass>30;score<100&pretty"
+ *     curl -u foo:bar -k -X GET "https://127.0.0.1:55000/configuration-assessment/000?offset=0&limit=2&q=pass>30;score<100&pretty"
  *
  */
 router.get('/:agent_id', cache(), function(req, res) {
     query_checks = {'name':'alphanumeric_param', 'description':'alphanumeric_param', 'references':'alphanumeric_param', 'hash':'alphanumeric_param'};
-    templates.array_request("/configuration_assessment/:agent_id", req, res, "configuration_assessment", {'agent_id':'numbers'}, query_checks);
+    templates.array_request("/configuration-assessment/:agent_id", req, res, "configuration_assessment", {'agent_id':'numbers'}, query_checks);
 })
 
 
 /**
- * @api {get} /configuration_assessment/:agent_id/checks/:id Get configuration_assessment checks database
+ * @api {get} /configuration-assessment/:agent_id/checks/:id Get configuration assessment checks database
  * @apiName GetPMAgentChecks
  * @apiGroup Info
  *
  * @apiParam {Number} [agent_id] Agent ID.
- * @apiParam {Number} [scan_id] Filters by scan id
+ * @apiParam {String} [policy_id] Filters by scan id
  * @apiParam {String} [title] Filters by title
  * @apiParam {String} [description] Filters by policy description
  * @apiParam {String} [rationale] Filters by rationale
@@ -62,20 +62,20 @@ router.get('/:agent_id', cache(), function(req, res) {
  * @apiParam {String} [sort] Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order.
  * @apiParam {String} [search] Looks for elements with the specified string.
  *
- * @apiDescription Returns the configuration_assessment checks of an agent
+ * @apiDescription Returns the configuration assessment checks of an agent
  *
  * @apiExample {curl} Example usage:
- *     curl -u foo:bar -k -X GET "https://127.0.0.1:55000/configuration_assessment/000/checks/1907428094?name=&pretty"
+ *     curl -u foo:bar -k -X GET "https://127.0.0.1:55000/configuration-assessment/000/checks/1907428094?name=&pretty"
  *
  */
-router.get('/:agent_id/checks/:scan_id', cache(), function(req, res) {
+router.get('/:agent_id/checks/:policy_id', cache(), function(req, res) {
     query_checks = {'title': 'alphanumeric_param', 'description': 'alphanumeric_param',
-        'rationale': 'alphanumeric_param', 'remediation': 'alphanumeric_param', 'file': 'paths', 'process': 'alphanumeric_param', 'directory': 'paths',
-        'registry': 'alphanumeric_param', 'references': 'alphanumeric_param', 'result': 'alphanumeric_param'
+        'rationale': 'alphanumeric_param', 'remediation': 'alphanumeric_param', 'file': 'paths', 'process': 'alphanumeric_param',
+        'directory': 'paths', 'registry': 'alphanumeric_param', 'references': 'alphanumeric_param', 'result': 'alphanumeric_param'
     };
-    templates.array_request("/configuration_assessment/:agent_id/checks/:scan_id", req, res,
+    templates.array_request("/configuration-assessment/:agent_id/checks/:policy_id", req, res,
                "configuration_assessment",
-               {'agent_id': 'numbers', 'scan_id': 'alphanumeric_param'}, query_checks);
+               {'agent_id': 'numbers', 'policy_id': 'alphanumeric_param'}, query_checks);
 })
 
 module.exports = router;
