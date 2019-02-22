6a39fe495cc06cc0669164f5e74bf562f31efa96
druizz90 <demetrio.ruiz@wazuh.com>
2019-02-22 10:37:13 +0100

    Regex for filtering paths was updated.

--- helpers/filters.js
+++ helpers/filters.js
@@ -67,7 +67,7 @@ exports.check_path = function(path, req, res) {
         return true
     }
 
-    re = new RegExp(/((^etc\/rules\/|^etc\/decoders\/)[A-Za-z_\-\/]+\.{1}xml|(^etc\/lists\/)[A-Za-z_\-\.\/]+)/)
+    re = new RegExp(/((^etc\/rules\/|^etc\/decoders\/)[\w_\-\/]+\.{1}xml|(^etc\/lists\/)[|w_\-\.\/]+)/)
     if (!re.test(path)) {
         res_h.bad_request(req, res, 704);
         return false
