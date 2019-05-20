de15b9f0034320bf4d8b202e7628aedbd29060a5
Alberto Marin <alberto.marin@wazuh.com>
2019-05-20 12:56:31 +0200

    Get rule gpg13 requirements

--- controllers/rules.js
+++ controllers/rules.js
@@ -156,6 +156,46 @@ router.get('/pci', cache(), function(req, res) {
 
 
 /**
+ * @api {get} /rules/gpg13 Get rule gpg13 requirements
+ * @apiName GetRulesGpg13
+ * @apiGroup Info
+ *
+ * @apiParam {Number} [offset] First element to return in the collection.
+ * @apiParam {Number} [limit=500] Maximum number of elements to return.
+ * @apiParam {String} [sort] Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order.
+ * @apiParam {String} [search] Looks for elements with the specified string.
+ *
+ * @apiDescription Returns the GPG13 requirements of all rules.
+ *
+ * @apiExample {curl} Example usage:
+ *     curl -u foo:bar -k -X GET "https://127.0.0.1:55000/rules/gpg13?offset=0&limit=10&pretty"
+ *
+ */
+router.get('/gpg13', cache(), function(req, res) {
+    logger.debug(req.connection.remoteAddress + " GET /rules/gpg13");
+
+    req.apicacheGroup = "rules";
+
+    var data_request = {'function': '/rules/gpg13', 'arguments': {}};
+    var filters = {'offset': 'numbers', 'limit': 'numbers', 'sort':'sort_param', 'search':'search_param'};
+
+    if (!filter.check(req.query, filters, req, res))  // Filter with error
+        return;
+
+    if ('offset' in req.query)
+        data_request['arguments']['offset'] = Number(req.query.offset);
+    if ('limit' in req.query)
+        data_request['arguments']['limit'] = Number(req.query.limit);
+    if ('sort' in req.query)
+        data_request['arguments']['sort'] = filter.sort_param_to_json(req.query.sort);
+    if ('search' in req.query)
+        data_request['arguments']['search'] = filter.search_param_to_json(req.query.search);
+
+    execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
+})
+
+
+/**
  * @api {get} /rules/gdpr Get rule gdpr requirements
  * @apiName GetRulesGdpr
  * @apiGroup Info
