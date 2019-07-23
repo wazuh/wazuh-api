786dc672248fe35f5bb61a778b0e6698f0c3fa4a
druizz90 <demetrio.ruiz@wazuh.com>
2019-07-23 13:42:05 +0200

    Add support for 'q' parameter to 'GET rules' endpoint.

--- controllers/rules.js
+++ controllers/rules.js
@@ -31,6 +31,8 @@ var router = require('express').Router();
  * @apiParam {String} [hipaa] Filters the rules by hipaa requirement.
  * @apiParam {String} [nist-800-53] Filters the rules by nist-800-53 requirement.
  * @apiParam {String} [gpg13] Filters the rules by gpg13 requirement.
+ * @apiParam {String} [q] Query to filter results by. For example q="name=worker-1"
+ *
  * @apiDescription Returns all rules.
  *
  * @apiExample {curl} Example usage:
@@ -38,51 +40,13 @@ var router = require('express').Router();
  *
  */
 router.get('/', cache(), function(req, res) {
-    logger.debug(req.connection.remoteAddress + " GET /rules");
+    param_checks = {}
+    query_checks = {'status':'alphanumeric_param', 'group':'alphanumeric_param',
+        'level':'ranges', 'path':'paths', 'file':'alphanumeric_param', 'pci':'alphanumeric_param',
+        'gdpr': 'alphanumeric_param', 'hipaa': 'alphanumeric_param',
+        'nist-800-53': 'alphanumeric_param', 'gpg13': 'alphanumeric_param'};
 
-    req.apicacheGroup = "rules";
-
-    var data_request = {'function': '/rules', 'arguments': {}};
-    var filters = {'offset': 'numbers', 'limit': 'numbers', 'sort':'sort_param',
-                   'search':'search_param', 'status':'alphanumeric_param',
-                   'group':'alphanumeric_param', 'level':'ranges', 'path':'paths',
-                   'file':'alphanumeric_param', 'pci':'alphanumeric_param',
-                   'gdpr': 'alphanumeric_param', 'hipaa': 'alphanumeric_param',
-                   'nist-800-53': 'alphanumeric_param', 'gpg13': 'alphanumeric_param'};
-
-    if (!filter.check(req.query, filters, req, res))  // Filter with error
-        return;
-
-    if ('offset' in req.query)
-        data_request['arguments']['offset'] = Number(req.query.offset);
-    if ('limit' in req.query)
-        data_request['arguments']['limit'] = Number(req.query.limit);
-    if ('sort' in req.query)
-        data_request['arguments']['sort'] = filter.sort_param_to_json(req.query.sort);
-    if ('search' in req.query)
-        data_request['arguments']['search'] = filter.search_param_to_json(req.query.search);
-    if ('status' in req.query)
-        data_request['arguments']['status'] = req.query.status;
-    if ('group' in req.query)
-        data_request['arguments']['group'] = req.query.group;
-    if ('level' in req.query)
-        data_request['arguments']['level'] = req.query.level;
-    if ('path' in req.query)
-        data_request['arguments']['path'] = req.query.path;
-    if ('file' in req.query)
-        data_request['arguments']['file'] = req.query.file;
-    if ('pci' in req.query)
-        data_request['arguments']['pci'] = req.query.pci;
-    if ('gdpr' in req.query)
-        data_request['arguments']['gdpr'] = req.query.gdpr;
-    if ('hipaa' in req.query)
-        data_request['arguments']['hipaa'] = req.query.hipaa;
-    if ('nist-800-53' in req.query)
-        data_request['arguments']['nist_800_53'] = req.query['nist-800-53'];
-    if ('gpg13' in req.query)
-        data_request['arguments']['gpg13'] = req.query.gpg13;
-
-    execute.exec(python_bin, [wazuh_control], data_request, function (data) { res_h.send(req, res, data); });
+    templates.array_request('/rules', req, res, "rules", param_checks, query_checks);
 })
 
 /**
