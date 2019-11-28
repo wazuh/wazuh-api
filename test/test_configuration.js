f7ea737248dad58e4e7256d3133d23d16cf20e4c
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2019-11-28 15:44:15 +0100

    Add mocha tests

--- test/test_configuration.js
+++ test/test_configuration.js
@@ -20,11 +20,15 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 describe('Configuration', function() {
 
     describe('PUT/configuration/validation/:type', function() {
-        agent_xml = fs.readFileSync('./test/data/agent.conf.xml', 'utf8')
+        agent_xml = fs.readFileSync('./test/data/agent.conf.xml', 'utf8');
+        invalid_agent_xml = fs.readFileSync('./test/data/invalid.conf.xml', 'utf8');
+        wrong_agent_xml = fs.readFileSync('./test/data/wrong.conf.xml', 'utf8');
+        ossec_xml = fs.readFileSync('./test/data/ossec.conf.xml', 'utf8');
+        invalid_ossec_xml = fs.readFileSync('./test/data/invalid.ossec.conf.xml', 'utf8');
 
         it('Request', function(done) {
             request(common.url)
-            .post("/configuration/validation/remote")
+            .put("/configuration/validation/remote")
             .auth(common.credentials.user, common.credentials.password)
             .set('Content-Type', 'application/xml')
             .send(agent_xml)
@@ -36,7 +40,88 @@ describe('Configuration', function() {
                 res.body.should.have.properties(['error', 'data']);
 
                 res.body.error.should.equal(0);
-                res.body.data.should.be.an.list;
+                res.body.data.status.should.equal('OK');
+                res.body.data.should.have.properties(['status', 'details']);
+                done();
+            });
+        });
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/configuration/validation/remote")
+            .auth(common.credentials.user, common.credentials.password)
+            .set('Content-Type', 'application/xml')
+            .send(invalid_agent_xml)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.status.should.equal('KO');
+                res.body.data.should.have.properties(['status', 'details']);
+                done();
+            });
+        });
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/configuration/validation/remote")
+            .auth(common.credentials.user, common.credentials.password)
+            .set('Content-Type', 'application/xml')
+            .send(wrong_agent_xml)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.status.should.equal('KO');
+                res.body.data.should.have.properties(['status', 'details']);
+                done();
+            });
+        });
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/configuration/validation/manager")
+            .auth(common.credentials.user, common.credentials.password)
+            .set('Content-Type', 'application/xml')
+            .send(ossec_xml)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.status.should.equal('OK');
+                res.body.data.should.have.properties(['status', 'details']);
+                done();
+            });
+        });
+
+        it('Request', function(done) {
+            request(common.url)
+            .put("/configuration/validation/manager")
+            .auth(common.credentials.user, common.credentials.password)
+            .set('Content-Type', 'application/xml')
+            .send(invalid_ossec_xml)
+            .expect("Content-type",/json/)
+            .expect(200)
+            .end(function(err,res){
+                if (err) return done(err);
+
+                res.body.should.have.properties(['error', 'data']);
+
+                res.body.error.should.equal(0);
+                res.body.data.status.should.equal('KO');
+                res.body.data.should.have.properties(['status', 'details']);
                 done();
             });
         });
